Fixed bug in the calculation of autocorrelation using complex input signal Improved test

unittest/ps_test.py
@@ -15,7 +15,7 @@ class TestCuda(unittest.TestCase):
 
     def test_acorrelation_float32(self):
 
-        data = np.array(self.data, dtype='float32')
+        data = np.array(self.data.real, dtype='float32')
 
         cuda_res = cuda_acorrelate(data, mode="same") / data.size
      #   cuda_res = cuda_fft(np.array(cuda_res, dtype='complex64'))
@@ -28,12 +28,12 @@ class TestCuda(unittest.TestCase):
 
     def test_ps_float64(self):
 
-        data = np.array(self.data, dtype='float64')
+        data = np.array(self.data.real, dtype='float64')
 
-        cuda_res = cuda_acorrelate(data, mode="same") / data.size
+        cuda_res = cuda_acorrelate(data, mode='valid') / data.size
         cuda_res = cuda_fft(np.array(cuda_res, dtype='complex128'))
 
-        numpy_res = np.correlate(data, data, mode='same') / data.size
+        numpy_res = np.correlate(data, data, mode='valid') / data.size
         numpy_res = np.fft.fft(numpy_res)
 
         self.assertEqual(np.allclose(cuda_res, numpy_res, rtol=1, atol=1.e-16), True)
@@ -44,10 +44,11 @@ class TestCuda(unittest.TestCase):
         data = np.array(self.data, dtype='complex64')
 
         cuda_res = cuda_acorrelate(data, mode="same") / data.size
-#        cuda_res = cuda_fft(cuda_res)
+        cuda_res = cuda_fft(cuda_res)
 
         numpy_res = np.correlate(data, data, mode='same') / data.size
-  #      numpy_res = np.fft.fft(numpy_res)
+        numpy_res = np.fft.fft(numpy_res)
+
 
         self.assertEqual(np.allclose(cuda_res, numpy_res, rtol=1, atol=1.e-8), True)
 
@@ -55,11 +56,11 @@ class TestCuda(unittest.TestCase):
 
         data = np.array(self.data, dtype='complex128')
 
-        cuda_res = cuda_acorrelate(data, mode="same") / data.size
-#        cuda_res = cuda_fft(cuda_res)
+        cuda_res = cuda_acorrelate(data, mode="full") / data.size
+        cuda_res = cuda_fft(cuda_res)
 
-        numpy_res = np.correlate(data, data, mode='same') / data.size
-   #     numpy_res = np.fft.fft(numpy_res)
+        numpy_res = np.correlate(data, data, mode='full') / data.size
+        numpy_res = np.fft.fft(numpy_res)
 
         print np.array_equal(cuda_res, numpy_res)
 